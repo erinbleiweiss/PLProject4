34cc3ae6876d2e3fb2b1ac2331fa2313682a15bb
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2016-03-24 20:20:02 -0500

    6 thru 8

--- Problem1/Problem1.py
+++ Problem1/Problem1.py
@@ -49,6 +49,20 @@ print "\nselect last_name, first_name, title, salary from s_emp where salary > 1
 print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept id > 40 order by salary desc: ", sorted([[i[1], i[2], i[6], i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40], key = lambda x: int(x[3]), reverse = True)
 
 #Erin
-# 8)
-print "select last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by salary desc;"
+# 5)
+print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by salary desc:"
 print (sorted([[i[1], i[2], i[6], i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40], key=lambda x: int(x[3]), reverse=True))
+
+# 6)
+print("\nselect last_name, first_name, title, salary, name from s_emp e join s_dept d on(e.dept_id = d.id)")
+print ([[i[1], i[2], i[6], i[7]] for i in s_emp[1::] for j in s_dept[1::] if i[9] == j[0]])
+
+# 7)
+print ("\nselect dept_id, avg(salary) from s_emp group by dept_id order by dept_id:")
+for department in { d[9] for d in s_emp[1::] }:
+    print (department, (lambda l: round(sum(l) / len(l), 2))(map(float,[ e[7] for e in s_emp[1::] if e[9] == department ])))
+
+# 8)
+print("\nselect dept_id, avg(salary) from s_emp group by dept_id having avg(salary) < 1500:")
+for department in { d[9] for d in s_emp[1::] }:
+     print (lambda deptno, avgSal: (deptno, avgSal) if avgSal < 1500 else '')(department, (lambda l: round(sum(l) / len(l), 2))(map(float,[ e[7] for e in s_emp[1::] if e[9] == department ])))
