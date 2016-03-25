9b8cbe4e371536999b8c9c35824d27cf1eb29fb2
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2016-03-24 21:25:11 -0500

    iterator proof of concept

--- Problem2/Lists.js
+++ Problem2/Lists.js
@@ -26,7 +26,7 @@ var list = function() {
                     l.currentNode = node;
                     l.length++;
                 }
-            },
+            }
         };
 
         var F = function () {
@@ -46,7 +46,7 @@ var list = function() {
                 l.length--;
             }
             return this;
-        }
+        };
         f.concat = f.cons = function(e){
             if (typeof e === 'string' || e instanceof String) {l.add(e);}
             else {
@@ -56,7 +56,7 @@ var list = function() {
                     n = n.next;
                 }
             }
-        }
+        };
         f.length = function(){return l.length};
         f.map = function(f){
             if (f instanceof Function) {
@@ -66,48 +66,88 @@ var list = function() {
                     n = n.next;
                 }
             }
-        }
+        };
+
+        f.iterate = function(){
+            var current;
+
+            return {
+                getNext: function () {
+                    if (current == null) {
+                        current = l.head;
+                    } else {
+                        current = current.next;
+                    }
+                    return current.data;
+                }
+            }
+
+        };
 
         return f;
     }();
     return list;
 };
 
+function myfunction(){
+
+}
+
+
 var l1 = new list();
-l1.concat('a')
-l1.cons('b')
-document.writeln("l1: " + l1.first() + "<BR>");
-document.writeln("l1: " + l1.length() + "<BR>");
+l1.concat('a');
+l1.cons('b');
+document.writeln("l1: " + l1.first() + "<br>");
+document.writeln("l1: " + l1.length() + "<br>");
 
 var l2 = new list();
-l2.cons('c')
-document.writeln("<BR>l2: " + l2.car() + "<BR>");
-document.writeln("l2: " + l2.length() + "<BR>");
+l2.cons('c');
+document.writeln("<br>l2: " + l2.car() + "<br>");
+document.writeln("l2: " + l2.length() + "<br>");
 
 var l3 = new list();
 var l4 = new list();
-l3.cons('x')
-l3.cons('y')
-l3.cons('z')
+
+l3.cons('x');
+l3.cons('y');
+l3.cons('z');
 l4.cons(l3);
 l4.cons(l3.car());
 
-document.writeln("<BR>l3: " + l3.car());
+document.writeln("<br>l3: " + l3.car());
 while(l3.length() > 0) {
     document.writeln(", " + l3.cdr().car());
 }
 
 var h = l4.run('head');
-document.writeln("<BR>l4: " + h.data);
+document.writeln("<br>l4: " + h.data);
 for(var i = 1; i < l4.length(); i++) {
     h = h.next;
     document.writeln(", " + h.data);
 }
 
-l4.map(function(x){return x+x})
+l4.map(function(x){return x+x});
 var h = l4.run('head');
-document.writeln("<BR>l4: " + h.data);
+document.writeln("<br>l4: " + h.data);
 for(var i = 1; i < l4.length(); i++) {
     h = h.next;
     document.writeln(", " + h.data);
 }
+
+var l5 = new list();
+l5.cons('a');
+l5.cons('b');
+l5.cons('c');
+l5.cons('d');
+l5.cons('e');
+l5.cons('f');
+
+var iterator = l5.iterate();
+document.writeln("<br>");
+document.writeln("<br>" + iterator.getNext());
+document.writeln("<br>" + iterator.getNext());
+document.writeln("<br>" + iterator.getNext());
+document.writeln("<br>" + iterator.getNext());
+document.writeln("<br>" + iterator.getNext());
+document.writeln("<br>" + iterator.getNext());
+document.writeln("<br>" + iterator.getNext());
