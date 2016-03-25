6849417cb89dabfb4b7e9367ac493539dbd497e2
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2016-03-24 21:27:41 -0500

    cleanup and formatting

--- Problem2/Lists.js
+++ Problem2/Lists.js
@@ -70,7 +70,6 @@ var list = function() {
 
         f.iterate = function(){
             var current;
-
             return {
                 getNext: function () {
                     if (current == null) {
@@ -81,7 +80,6 @@ var list = function() {
                     return current.data;
                 }
             }
-
         };
 
         return f;
@@ -89,61 +87,16 @@ var list = function() {
     return list;
 };
 
-function myfunction(){
-
-}
-
 
 var l1 = new list();
-l1.concat('a');
+l1.cons('a');
 l1.cons('b');
-document.writeln("l1: " + l1.first() + "<br>");
-document.writeln("l1: " + l1.length() + "<br>");
+l1.cons('c');
+l1.cons('d');
+l1.cons('e');
+l1.cons('f');
 
-var l2 = new list();
-l2.cons('c');
-document.writeln("<br>l2: " + l2.car() + "<br>");
-document.writeln("l2: " + l2.length() + "<br>");
-
-var l3 = new list();
-var l4 = new list();
-
-l3.cons('x');
-l3.cons('y');
-l3.cons('z');
-l4.cons(l3);
-l4.cons(l3.car());
-
-document.writeln("<br>l3: " + l3.car());
-while(l3.length() > 0) {
-    document.writeln(", " + l3.cdr().car());
-}
-
-var h = l4.run('head');
-document.writeln("<br>l4: " + h.data);
-for(var i = 1; i < l4.length(); i++) {
-    h = h.next;
-    document.writeln(", " + h.data);
-}
-
-l4.map(function(x){return x+x});
-var h = l4.run('head');
-document.writeln("<br>l4: " + h.data);
-for(var i = 1; i < l4.length(); i++) {
-    h = h.next;
-    document.writeln(", " + h.data);
-}
-
-var l5 = new list();
-l5.cons('a');
-l5.cons('b');
-l5.cons('c');
-l5.cons('d');
-l5.cons('e');
-l5.cons('f');
-
-var iterator = l5.iterate();
-document.writeln("<br>");
+var iterator = l1.iterate();
 document.writeln("<br>" + iterator.getNext());
 document.writeln("<br>" + iterator.getNext());
 document.writeln("<br>" + iterator.getNext());
